4a9b7d5124ecae0830622711d309d20307a6b4c9
imzhangxian <imzhangxian@gmail.com>
2024-08-21 14:49:41 +0800

    ! Fixed target dimension mismatch !

--- hw3/cs285/agents/dqn_agent.py
+++ hw3/cs285/agents/dqn_agent.py
@@ -80,13 +80,13 @@ class DQNAgent(nn.Module):
             else:
                 next_action = next_qa_values.argmax(dim=1) # batch_size x 1
             
-            next_q_values = next_qa_values.gather(-1, next_action.unsqueeze(1))
-            target_values = reward + self.discount * next_q_values * ~done
+            next_q_values = next_qa_values.gather(-1, next_action.view(batch_size, 1))
+            target_values = reward.view(batch_size, 1) + self.discount * next_q_values * ~ done.view(batch_size, 1)
 
         # TODO(student): train the critic with the target values
         # !! need to keep the computation graph !!
         qa_values = self.critic(obs)
-        q_values = qa_values.gather(-1, action.unsqueeze(1)) # Compute from the data actions; see torch.gather
+        q_values = qa_values.gather(-1, action.view(batch_size, 1)) # Compute from the data actions; see torch.gather
         # mse = nn.MSELoss()
         loss = nn.MSELoss()(q_values, target_values)
 
